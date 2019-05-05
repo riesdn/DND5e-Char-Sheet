17e285db5ae6c42b969446c19c16c406875e7d27
Danae Ries <42923721+riesdn@users.noreply.github.com>
2019-05-04 22:04:09 -0400

    Updated dndStats.js
    - found glitch where points can be added or subtracted from base stats even when availablePts is max or min
    - added update to show user the number of points assigned to each base stat
    - formatting updates for readability
    - updated Features to Add list

--- dndStats.js
+++ dndStats.js
@@ -3,7 +3,7 @@ function editPoints(statBtn, availablePts) {
 	
 	console.log("button was clicked, editPoints() function activated");
 	console.log("total available points: " + availablePts);
-	var operator, statsBar, statsPts;
+	var operator, statsBar, statsPts, modifier;
   
 	operator = statBtn.text();
 	console.log("var operator = " + operator);
@@ -12,6 +12,11 @@ function editPoints(statBtn, availablePts) {
 	statsPts = statsBar.val();
 	console.log("var statsPts = " + statsPts);
 	
+  /* 
+    ***** ADD A CONDITIONAL FOR CHECKING AVAILABLE POINTS !!!! *****
+    -> it's glitching so that all 100 points will be used but you 
+       can keep adding more points to that stat bar. 
+  */
   if (operator === "+" && statsPts < 20) {
     statsPts++;
     availablePts--;
@@ -22,16 +27,33 @@ function editPoints(statBtn, availablePts) {
   
   statsBar.val(statsPts);
   $("#availablePts").val(availablePts);
-  $("#avPtsText").val(availablePts + "/100");
+  $("#avPtsText").val(availablePts + "/75");
   console.log("modified statsPts = " + statsPts);
   
-  // put current points in points column
+  // put current points in points column, dont need to use a for loop bc of THIS keyword
+  statBtn.closest("tr").find("#statsPts").val(statsPts);
+  
+/*
+
+  THIS IS NOT THE CORRECT RESULT, MAY NEED ANOTHER VARIABLE
+
   // take current points and translate them into general stats modifier scores
+  // To determine an ability modifier without consulting the table, subtract 10 from the 
+     ability score and then divide the result by 2 and round down.
+  modifier = (statsPts - 10) / 2; // subtract 10 then divide by 2
+  console.log("modifier value is: " + modifier);
+  // determine if we need to round down
+  if (modifier % 2 == 1) {
+    modifier -= 0.5;
+    console.log("modifier value rounded to: " + modifier);
+  } 
+  
+*/
 
 } // END editPoints()
 
 $(document).ready(function(){
-	
+  
 	$("button").click(function() {
 		editPoints($(this), $("#availablePts").val());
 	});
@@ -40,8 +62,14 @@ $(document).ready(function(){
 
 /* 
   FEATURES TO ADD:
-  - BASE STATS points change automatically effects all skill modifiers as well and autopopulates in each skill's modifiers text box
+  - BASE STATS points change automatically effects all skill modifiers as 
+      well and autopopulates in each skill's modifiers text box
   - RACE selection changes base stat points available
-    > preferrably shown in different color than assigned points??
-  - CLASS selection changes skill modifiers and if a skill is marked with proficiency or not (Seperate from base mods from base stats / in addition to mods from base stats)
+    > preferrably shown in different color than assigned points?? maybe??
+  - CLASS selection changes skill modifiers and if a skill is marked with 
+      proficiency or not (Seperate from base mods from base stats / in 
+      addition to mods from base stats)
+  - DOWNLOAD TO PDF
+    > currently (May 2019) user has nothing to do with this information 
+      other than copy it down by hand
 */
